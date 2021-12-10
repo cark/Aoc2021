bdfa672abc517542c4d418754f2739d96b760cb5
cark <carkhy@gmail.com>
2021-12-10 18:19:42 +0100

    Cleanup
    Signed-off-by: cark <carkhy@gmail.com>

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,125 +4,96 @@ const FILENAME: &str = "input.txt";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string(FILENAME)?;
-    let data: Vec<&str> = input.lines().collect();
-    let bit_stats = BitStats::parse(&data);
-    let (gamma_rate, epsilon_rate) = part1(&bit_stats);
+    let lines: Vec<&str> = input.lines().collect();
+    let values = Values::parse(&lines);
 
+    let (gamma_rate, epsilon_rate) = part1(&values);
     println!("part1 : {}", gamma_rate * epsilon_rate);
 
-    let (oxy, co2) = part2(&bit_stats);
+    let (oxy, co2) = part2(&values);
     println!("part2 : {}", oxy * co2);
-    
+
     Ok(())
 }
 
-struct BitStats {
-    bits: Vec<u32>,
+struct Values {
+    bit_count: usize,
     values: Vec<u32>,
 }
 
-impl BitStats {
-    fn parse(data: &[&str]) -> Self {
-        let mut result = Self {
-            bits: Vec::new(),
-            values: Vec::new(),
-        };
-        for &line in data {
-            let mut value = 0;
-            for (order, char) in line.chars().enumerate() {
-                if result.bits.len() < order + 1 {
-                    result.bits.resize(order + 1, 0);
-                }
-                value <<= 1;
-                if char == '1' {
-                    result.bits[order] += 1;
-                    value += 1 ;
-                }                
-            }
-            result.values.push(value);
-        }
-        result
+impl Values {
+    fn parse(lines: &[&str]) -> Self {
+        let mut bit_count = 0;
+        let values = lines
+            .iter()
+            .map(|&line| {
+                line.chars().enumerate().fold(0, |value, (order, char)| {
+                    bit_count = bit_count.max(order + 1);
+                    (value << 1) + if char == '1' { 1 } else { 0 }
+                })
+            })
+            .collect();
+        Self { bit_count, values }
     }
 
     fn mask(&self) -> u32 {
         let mut result = 0;
-        for i in 0..self.bits.len() {
+        for i in 0..self.bit_count {
             result += 1_u32 << i;
         }
         result
     }
-
-    // order is 0 for left-most bit
-    fn most_common_bit(&self, order: usize, prefer_bit: bool) -> u32 {
-        match self.bits[order].cmp(&(self.values.len() as u32 / 2)) {
-            std::cmp::Ordering::Less => 0,
-            std::cmp::Ordering::Equal => match prefer_bit {
-                true => 1,
-                false => unreachable!("Nothing was said for equal values"),
-            },
-            std::cmp::Ordering::Greater => 1,
-        }
-    }
 }
 
-fn part1(bit_stats: &BitStats) -> (u32, u32) {
+fn part1(values: &Values) -> (u32, u32) {
     let mut gamma_rate: u32 = 0;
-    for order in 0..bit_stats.bits.len() {
-        gamma_rate += bit_stats.most_common_bit(bit_stats.bits.len() - order - 1, false) << order as u32;
+    for order in 0..values.bit_count {
+        gamma_rate += most_common(&values.values, order) << order as u32;
     }
-    let epsilon_rate: u32 = !gamma_rate & bit_stats.mask();
+    let epsilon_rate: u32 = !gamma_rate & values.mask();
     (gamma_rate, epsilon_rate)
 }
 
-
-fn match_shift(value: u32, shift: usize) -> bool {
-    ((value >> shift) & 1) == 1
-}
-
-fn count_bits(in_vec: &[u32], shift: usize) -> u32 {    
-    in_vec.iter().filter(|&v| match_shift(*v, shift)).count() as u32
-}
-
 fn most_common(in_vec: &[u32], shift: usize) -> u32 {
-    println!("yoh {} {}", count_bits(in_vec, shift), (in_vec.len() as u32 / 2));
     match (count_bits(in_vec, shift) as f32).partial_cmp(&(in_vec.len() as f32 / 2.0)) {
         Some(a) => match a {
             std::cmp::Ordering::Less => 0,
             std::cmp::Ordering::Equal => 1,
             std::cmp::Ordering::Greater => 1,
         },
-        None => todo!(),
+        None => unreachable!(),
     }
 }
 
-fn invert_bit(bit: u32) -> u32 {
-    !bit & 1
+fn count_bits(in_vec: &[u32], shift: usize) -> u32 {
+    in_vec.iter().filter(|&v| match_shift(*v, shift)).count() as u32
 }
 
-fn part2(bit_stats: &BitStats) -> (u32, u32) {
-    // oxygen-rating
-    let mut values = bit_stats.values.clone();
-    let mut order = 0;
-    while values.len() > 1 {
-        let shift = bit_stats.bits.len() - order - 1;        
-        let most_common = most_common(&values, shift);   //bit_stats.most_common_bit(order, true);
-        values = values.into_iter().filter(|&v| ((v >> shift) & 1) == most_common).collect();            
-        order += 1;
-    }
-    let oxy = values[0];
-    
-    // co2-rating
-    let mut values = bit_stats.values.clone();
-    let mut order = 0;
-    while values.len() > 1 {
-        let shift = bit_stats.bits.len() - order - 1;        
-        let most_common = invert_bit(most_common(&values, shift));
-        values = values.into_iter().filter(|&v| ((v >> shift) & 1) == most_common).collect();            
-        order += 1;
-    }
-    let co2 = values[0];
+fn match_shift(value: u32, shift: usize) -> bool {
+    ((value >> shift) & 1) == 1
+}
 
-    (oxy,co2)
+fn part2(values: &Values) -> (u32, u32) {
+    (
+        life_support_rating(values, false),
+        life_support_rating(values, true),
+    )
+}
+
+fn life_support_rating(bit_stats: &Values, is_co2 : bool) -> u32 {
+    let mut values = bit_stats.values.clone();
+    let mut order = 0;
+    while values.len() > 1 {
+        let shift = bit_stats.bit_count - order - 1;
+        let mut most_common = most_common(&values, shift);
+        if is_co2 { most_common = !most_common & 1}
+        values = values
+            .into_iter()
+            .filter(|&v| ((v >> shift) & 1) == most_common)
+            .collect();
+        order += 1;
+    }
+    values[0]
 }
 
 #[cfg(test)]
@@ -144,7 +115,7 @@ mod testing {
     #[test]
     fn part1_test() {
         let data: Vec<&str> = DATA.lines().collect();
-        let bit_stats = BitStats::parse(&data);
+        let bit_stats = Values::parse(&data);
         let (gamma_rate, epsilon_rate) = part1(&bit_stats);
         assert_eq!(gamma_rate, 22);
         assert_eq!(epsilon_rate, 9);
@@ -165,27 +136,24 @@ mod testing {
     #[test]
     fn part2_test() {
         let data: Vec<&str> = DATA.lines().collect();
-        let bit_stats = BitStats::parse(&data);
+        let bit_stats = Values::parse(&data);
         let (oxy, co2) = part2(&bit_stats);
-        
-        // let (gamma_rate, epsilon_rate) = part1(&bit_stats);
+
         assert_eq!(oxy, 23);
         assert_eq!(co2, 10);
-        // assert_eq!(epsilon_rate, 9);
-        // assert_eq!(gamma_rate * epsilon_rate, 198);
     }
     #[test]
     fn test_match_shift() {
-        assert_eq!(match_shift(0b1000,3), true);
-        assert_eq!(match_shift(0b1000,4), false);
-        assert_eq!(match_shift(0b1000,2), false);
-        assert_eq!(match_shift(0b1000,1), false);
-        assert_eq!(match_shift(0b1000,0), false);
-        assert_eq!(match_shift(0b0100,3), false);
-        assert_eq!(match_shift(0b0100,4), false);
-        assert_eq!(match_shift(0b0100,2), true);
-        assert_eq!(match_shift(0b0100,1), false);
-        assert_eq!(match_shift(0b0100,0), false);
+        assert_eq!(match_shift(0b1000, 3), true);
+        assert_eq!(match_shift(0b1000, 4), false);
+        assert_eq!(match_shift(0b1000, 2), false);
+        assert_eq!(match_shift(0b1000, 1), false);
+        assert_eq!(match_shift(0b1000, 0), false);
+        assert_eq!(match_shift(0b0100, 3), false);
+        assert_eq!(match_shift(0b0100, 4), false);
+        assert_eq!(match_shift(0b0100, 2), true);
+        assert_eq!(match_shift(0b0100, 1), false);
+        assert_eq!(match_shift(0b0100, 0), false);
     }
     #[test]
     fn test_count_bits() {
@@ -197,21 +165,19 @@ mod testing {
         assert_eq!(most_common(&[0b1000, 0b0111, 0b1001, 0b0011], 2), 0);
         assert_eq!(most_common(&[0b1000, 0b0111, 0b1001, 0b0011], 1), 1);
         assert_eq!(most_common(&[0b1000, 0b0111, 0b1001, 0b0011], 0), 1);
-        assert_eq!(count_bits(&[0b11110,
-                                 0b10110,
-                                 0b10111,
-                                 0b10101,
-                                 0b11100,
-                                 0b10000,
-                                 0b11001,], 3), 3);
-        assert_eq!(most_common(&[0b11110,
-                                 0b10110,
-                                 0b10111,
-                                 0b10101,
-                                 0b11100,
-                                 0b10000,
-                                 0b11001,]
-                               ,3),0)
-        // assert_eq!(most_common(&[0b1000, 0b0111, 0b1001, 0b0011], 3), 1);
+        assert_eq!(
+            count_bits(
+                &[0b11110, 0b10110, 0b10111, 0b10101, 0b11100, 0b10000, 0b11001,],
+                3
+            ),
+            3
+        );
+        assert_eq!(
+            most_common(
+                &[0b11110, 0b10110, 0b10111, 0b10101, 0b11100, 0b10000, 0b11001,],
+                3
+            ),
+            0
+        )
     }
 }
